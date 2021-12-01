e535c4a6492b187b765a9598e309e9d87989ec2d
“jungkeunmo“ <abc59684495@gmail.com>
2021-12-01 09:59:28 +0900

    wow

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -5,8 +5,29 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
     res.render("screens/home");
 });
+
 router.get("/login" , (req,res,next)=>{
     res.render("screens/login");
+});
+
+router.get("/help", (req, res, next) => {
+    res.render("screens/help");
+});
+
+router.get("/partcipants", (req, res, next) => {
+    res.render("screens/partcipants");
+});
+
+router.get("/notice", (req, res, next) => {
+    res.render("screens/notice");
+});
+
+router.get("/event", (req, res, next) => {
+    res.render("screens/event");
+});
+
+router.get("/termsofuse", (req, res, next) => {
+    res.render("screens/termsofuse");
 });
 
 module.exports = router;
