9bce09a386521bcf88d70a53a02f5f00cdb059b1
ghdehrl12345 <4leaf.edu.hdk@gmail.com>
2021-12-11 14:39:09 +0900

    webtoons

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -20,26 +20,26 @@ router.get("/", checkLogin, (req, res, next) => {
 router.get("/signin", (req, res, next) => {
   res.render("screens/signin");
 });
-router.post("/signin", (req,res)=>{
-    const loginQuery = `
+router.post("/signin", (req, res) => {
+  const loginQuery = `
     SELECT email,
            password
       FROM users
      WHERE email = "${req.body.email}"
        AND password = "${req.body.password}"  
     `;
-    db.query(loginQuery,(error,result)=>{
-        if(error) {
-            console.error(error);
-            return res.status(400).send("로그인 실패");
-        }else{
-            if(email = req.body.email , password = req.body.password) {
-                return res.status(200).send("로그인 성공");
-            }else{
-                return res.status(403).send("로그인 실패");
-            }
-        }
-    })
+  db.query(loginQuery, (error, result) => {
+    if (error) {
+      console.error(error);
+      return res.status(400).send("로그인 실패");
+    } else {
+      if (((email = req.body.email), (password = req.body.password))) {
+        return res.status(200).send("로그인 성공");
+      } else {
+        return res.status(403).send("로그인 실패");
+      }
+    }
+  });
 });
 
 router.get("/signup", (req, res, next) => {
@@ -47,21 +47,20 @@ router.get("/signup", (req, res, next) => {
 });
 
 router.post("/signup", (req, res, next) => {
-
-    const userCheckQuery=`
+  const userCheckQuery = `
         SELECT email
           FROM users
          WHERE email="${req.body.email}"`;
-        
-    db.query(userCheckQuery,(error,result)=>{
-        if(error) {
-            console.log(error);
-            return res.status(403).send("다시 시도해주세요");
-        }else{
-            if(result.length > 0){
-                return res.status(403).send("이메일 이미 존재합니다.");
-            }else{
-                const signupQuery = `
+
+  db.query(userCheckQuery, (error, result) => {
+    if (error) {
+      console.log(error);
+      return res.status(403).send("다시 시도해주세요");
+    } else {
+      if (result.length > 0) {
+        return res.status(403).send("이메일 이미 존재합니다.");
+      } else {
+        const signupQuery = `
                     INSERT INTO users (
                         name,
                         birth,
@@ -78,19 +77,17 @@ router.post("/signup", (req, res, next) => {
                         "${req.body.password}"
                 )`;
 
-                db.query(signupQuery,(error,resutl)=>{
-                    if(error){
-                        console.error(error);
-                        return res.status(400).send("회원가입 실패");
-                    }else{
-                        res.status(201).send("회원가입 성공")
-                    }
-                })
-            }
-        }
-    })
-
-
+        db.query(signupQuery, (error, resutl) => {
+          if (error) {
+            console.error(error);
+            return res.status(400).send("회원가입 실패");
+          } else {
+            res.status(201).send("회원가입 성공");
+          }
+        });
+      }
+    }
+  });
 });
 
 router.get("/help", (req, res, next) => {
@@ -113,7 +110,4 @@ router.get("/termsofuse", (req, res, next) => {
   res.render("screens/termsofuse");
 });
 
-router.get("/naverwebtoon", (req, res, next) => {
-  res.render("screens/naverwebtoon");
-});
 module.exports = router;
--- routers/webtoonRouter.js
+++ routers/webtoonRouter.js
@@ -4,22 +4,152 @@ const db = require("../db");
 const router = express.Router();
 
 router.get("/finished", (req, res, next) => {
-  const webtoonsSelectQuery = `
-    SELECT    id,
-              title,
-              score,
-              episode,
-              being
-      FROM    webtoons
+  const finishedWebtoonsSelectQuery = `
+  SELECT  id,
+          title,
+          score,
+          episode,
+          being,
+          publish
+    FROM  webtoons
+   WHERE  being		LIKE	'완결'
   `;
   try {
-    db.query(webtoonsSelectQuery, (err, webtoons) => {
+    db.query(finishedWebtoonsSelectQuery, (err, finishedWebtoons) => {
       console.log(err);
-      res.render("screens/finished", { webtoons });
+      res.render("screens/finished", { finishedWebtoons });
     });
   } catch (error) {
     console.error(error);
     return res.redirect("/");
+  }
+});
+
+router.get("/kakaoWeb", (req, res, next) => {
+  const kakaoWebtoonsSelectQuery = `
+  SELECT  id,
+		      title,
+		      score,
+		      episode,
+		      being,
+		      publish,
+          days
+    FROM  webtoons
+   WHERE  publish		LIKE	'카카오웹툰'
+     AND  being		  LIKE	'연재'
+  `;
+  try {
+    db.query(kakaoWebtoonsSelectQuery, (err, kakaoWebtoons) => {
+      console.log(err);
+      res.render("screens/kakaoWeb", { kakaoWebtoons });
+    });
+  } catch (error) {
+    console.error(error);
+    return res.redirect("/");
+  }
+});
+
+router.get("/kakaoPage", (req, res, next) => {
+  const kakaoPagesSelectQuery = `
+  SELECT  id,
+		      title,
+		      score,
+		      episode,
+		      being,
+		      publish,
+          days
+    FROM  webtoons
+   WHERE  publish		LIKE	'카카오페이지'
+     AND  being		  LIKE	'연재'
+  `;
+  try {
+    db.query(kakaoPagesSelectQuery, (err, kakaoPages) => {
+      console.log(err);
+      res.render("screens/kakaoPage", { kakaoPages });
+    });
+  } catch (error) {
+    console.error(error);
+    return res.redirect("/");
+  }
+});
+
+router.get("/naverWeb", (req, res, next) => {
+  const naverWebsSelectQuery = `
+  SELECT  id,
+		      title,
+		      score,
+		      episode,
+		      being,
+		      publish,
+          days
+    FROM  webtoons
+   WHERE  publish		LIKE	'네이버웹툰'
+     AND  being		  LIKE	'연재'
+  `;
+  try {
+    db.query(naverWebsSelectQuery, (err, naverWebtoons) => {
+      console.log(err);
+      res.render("screens/naverWeb", { naverWebtoons });
+    });
+  } catch (error) {
+    console.error(error);
+    return res.redirect("/");
+  }
+});
+
+router.get("/naverSeries", (req, res, next) => {
+  const naverSeriesSelectQuery = `
+  SELECT  id,
+		      title,
+		      score,
+		      episode,
+		      being,
+		      publish,
+          days
+    FROM  webtoons
+   WHERE  publish		LIKE	'네이버시리즈'
+     AND  being		  LIKE	'연재'
+  `;
+  try {
+    db.query(naverSeriesSelectQuery, (err, naverSeries) => {
+      console.log(err);
+      res.render("screens/naverSeries", { naverSeries });
+    });
+  } catch (error) {
+    console.error(error);
+    return res.redirect("/");
+  }
+});
+
+router.post("/webtoonsCreate", (req, res) => {
+  const webtoonsInsertQuery = `    
+      INSERT INTO reviews (
+        title,
+        score,
+        episode,
+        being,
+        publish,
+        days
+      )	VALUES	(
+        "${req.body.title}",
+        ${req.body.score},
+        ${req.body.episode},
+        "${req.body.being}"
+        "${req.body.publish}",
+        "${req.body.days}"
+      )
+      `;
+
+  try {
+    db.query(webtoonsInsertQuery, (error, CreWebtoons) => {
+      if (error) {
+        console.error(error);
+      }
+      res.redirect("/createWebtoon");
+    });
+  } catch (error) {
+    console.error(error);
+    res.redirect("/");
   }
 });
 
